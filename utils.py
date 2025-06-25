80e88d96c1e3a6a54ce6dc95d9ed570ed0bbdc0f
phonghocode <ntp11892vt@gmail.com>
2025-06-25 15:37:34 +0700

    training phase 1

--- utils.py
+++ utils.py
@@ -2,8 +2,47 @@ import string
 from underthesea import word_tokenize
 import os
 import json
-import py_vncorenlp
 from vncorenlp import VnCoreNLP
+
+import os
+import shutil
+
+
+def download_model(save_dir='./'):
+    # current_path = os.path.abspath(os.getcwd())
+    if save_dir[-1] == '/':
+        save_dir = save_dir[:-1]
+    if os.path.isdir(save_dir + "/models") and os.path.exists(save_dir + '/VnCoreNLP-1.2.jar'):
+        print("VnCoreNLP model folder " + save_dir + " already exists! Please load VnCoreNLP from this folder!")
+    else:
+        os.mkdir(save_dir + "/models")
+        os.mkdir(save_dir + "/models/dep")
+        os.mkdir(save_dir + "/models/ner")
+        os.mkdir(save_dir + "/models/postagger")
+        os.mkdir(save_dir + "/models/wordsegmenter")
+        # jar
+        os.system("wget https://raw.githubusercontent.com/vncorenlp/VnCoreNLP/master/VnCoreNLP-1.2.jar")
+        shutil.move("VnCoreNLP-1.2.jar", save_dir + "/VnCoreNLP-1.2.jar")
+        # wordsegmenter
+        os.system("wget https://raw.githubusercontent.com/vncorenlp/VnCoreNLP/master/models/wordsegmenter/vi-vocab")
+        os.system(
+            "wget https://raw.githubusercontent.com/vncorenlp/VnCoreNLP/master/models/wordsegmenter/wordsegmenter.rdr")
+        shutil.move("vi-vocab", save_dir + "/models/wordsegmenter/vi-vocab")
+        shutil.move("wordsegmenter.rdr", save_dir + "/models/wordsegmenter/wordsegmenter.rdr")
+        # postagger
+        os.system("wget https://raw.githubusercontent.com/vncorenlp/VnCoreNLP/master/models/postagger/vi-tagger")
+        shutil.move("vi-tagger", save_dir + "/models/postagger/vi-tagger")
+        # ner
+        os.system("wget https://raw.githubusercontent.com/vncorenlp/VnCoreNLP/master/models/ner/vi-500brownclusters.xz")
+        os.system("wget https://raw.githubusercontent.com/vncorenlp/VnCoreNLP/master/models/ner/vi-ner.xz")
+        os.system(
+            "wget https://raw.githubusercontent.com/vncorenlp/VnCoreNLP/master/models/ner/vi-pretrainedembeddings.xz")
+        shutil.move("vi-500brownclusters.xz", save_dir + "/models/ner/vi-500brownclusters.xz")
+        shutil.move("vi-ner.xz", save_dir + "/models/ner/vi-ner.xz")
+        shutil.move("vi-pretrainedembeddings.xz", save_dir + "/models/ner/vi-pretrainedembeddings.xz")
+        # parse
+        os.system("wget https://raw.githubusercontent.com/vncorenlp/VnCoreNLP/master/models/dep/vi-dep.xz")
+        shutil.move("vi-dep.xz", save_dir + "/models/dep/vi-dep.xz")
 
 number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
 chars = ["a", "b", "c", "d", "đ", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o"]
@@ -36,7 +75,7 @@ def load_json(path):
 
 def word_segmentation(text, save_dir='./vncorenlp'):
     os.makedirs(save_dir, exist_ok=True)
-    py_vncorenlp.download_model(save_dir=save_dir)
+    download_model(save_dir=save_dir)
     rdrsegmenter = VnCoreNLP("./vncorenlp/VnCoreNLP-1.2.jar", annotators="wseg", max_heap_size='-Xmx2g')
     word_sentences = rdrsegmenter.word_segment(text)
     if len(word_sentences) > 0:
