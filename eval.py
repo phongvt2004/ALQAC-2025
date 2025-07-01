f6eeae1d5fc44c1bb87185adfcbc84f0b8bdf606
phonghocode <ntp11892vt@gmail.com>
2025-07-01 16:25:03 +0700

    eval round 1

--- eval.py
+++ eval.py
@@ -7,32 +7,32 @@ import argparse
 import os
 import pickle
 import glob
-from utils import bm25_tokenizer
-
+from utils import bm25_tokenizer, calculate_f2
+import random
 from sentence_transformers import SentenceTransformer, util
 
-def encode_legal_data(legal_dict_json, model):
+def encode_legal_data(data_path, models):
     # print(legal_dict_json)
-    doc_data = json.load(open(legal_dict_json))
+    corpus = json.load(open(os.path.join(data_path, "corpus.json")))
     # print(len(doc_data))
-    list_emb_model = []
-    for model in model:
+    list_emb_models = []
+    for model in models:
         emb2_list = []
-        for k, doc in tqdm(doc_data.items()):
-            emb2 = model.encode(doc_data[k]["title"] + " " + doc_data[k]["text"])
+        for k, doc in tqdm(corpus.items()):
+            emb2 = model.encode(doc)
             emb2_list.append(emb2)
         emb2_arr = np.array(emb2_list)
-        list_emb_model.append(emb2_arr)
-    return list_emb_model
+        list_emb_models.append(emb2_arr)
+    return list_emb_models
 
-def encode_question(question_data, model):
+def encode_question(question_data, models):
     print("Start encoding questions.")
     question_embs = []
-    for model in model:
+    for model in models:
         emb_quest_dict = {}
-        for _, item in tqdm(enumerate(question_data)):
-            question_id = item["question_id"]
-            question = item["question"]
+        for qid, q in tqdm(question_data.items()):
+            question_id = qid
+            question = q
             emb_quest_dict[question_id] = model.encode(question)
         question_embs.append(emb_quest_dict)
     return question_embs
@@ -48,33 +48,41 @@ def load_bm25(bm25_path):
         bm25 = pickle.load(bm_file)
     return bm25
 
-def load_model(model_name):
-    model = SentenceTransformer(model_name)
-    return model
+def load_models(root, model_names):
+    models = []
+    for model_path in tqdm(model_names):
+        model_path = os.path.join(args.saved_model, model_path)
+        models.append(SentenceTransformer(model_path))
+    return models
 
-def load_question_json(question_path):
-    question_path = glob.glob(f"{question_path}/*.json")[0]
-    question_data = json.load(open(question_path))
+def load_question_json(data_path):
+    question_data = json.load(open(os.path.join(data_path, "queries.json")))
     return question_data
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data", default="", type=str, help="for loading question")
-    parser.add_argument("--raw_data", default="ALQAC_2025_data", type=str)
+    parser.add_argument("--raw_data", default="zac2021-ltr-data", type=str)
     parser.add_argument("--saved_model", default="saved_model", type=str)
-    parser.add_argument("--legal_dict_json", default="generated_data/legal_dict.json", type=str)
     parser.add_argument("--bm25_path", default="saved_model/bm25_Plus_04_06_model_full_manual_stopword", type=str)
     parser.add_argument("--legal_data", default="saved_model/doc_refers_saved", type=str, help="path to legal corpus for reference")
     parser.add_argument("--range-score", default=2.6, type=float, help="range of cos sin score for multiple-answer")
     parser.add_argument("--encode_legal_data", action="store_true", help="for legal data encoding")
+    parser.add_argument("--hybrid", action="store_true", help="for legal data encoding")
     args = parser.parse_args()
+
+    # define path to model
+    
+    model_names = []
+
     print("Start loading model.")
-    model = load_model(args.model_name)
+    models = load_models(args.saved_model, model_names)
+    print("Number of pretrained models: ", len(models))
 
     # load question from json file
-    question_items = load_question_json(args.data)["items"]
-    
+    question_items = load_question_json(args.raw_data)
+    train_path = os.path.join(args.raw_data, "alqac25_train.json")
+    data = json.load(open(train_path))
     print("Number of questions: ", len(question_items))
     
     # load bm25 model 
@@ -85,30 +93,50 @@ if __name__ == "__main__":
         doc_refers = pickle.load(doc_refer_file)
     # load pre encoded for legal corpus
     if args.encode_legal_data:
-        emb_legal_data = encode_legal_data(args.legal_dict_json, model)
+        emb_legal_data = encode_legal_data(args.raw_data, models)
     else:
         emb_legal_data = load_encoded_legal_corpus('encoded_legal_data.pkl')
 
     # encode question for query
-    question_embs = encode_question(question_items, model)
+    question_embs = encode_question(question_items, models)
 
     # define top n for compare and range of score
     top_n = 61425
     range_score = args.range_score
 
     pred_list = []
-
+    with open(os.path.join(args.data_path, "queries.json"), "r") as f:
+        queries = json.load(f)
+    qid_list = list(queries.keys())
+    random.seed(42)
+    random.shuffle(qid_list)
+    num_eval = int(len(qid_list) * args.eval_size)
+    eval_qid = qid_list[:num_eval]
     print("Start calculating results.")
-    for idx, item in tqdm(enumerate(question_items)):
+    for idx, item in tqdm(enumerate(data)):
         question_id = item["question_id"]
+        if question_id not in eval_qid:
+            continue
         question = item["question"]
+        relevant_articles = item["relevant_articles"]
+        actual_positive = len(relevant_articles)
+        
+        tokenized_query = bm25_tokenizer(question)
+        doc_scores = bm25.get_scores(tokenized_query)
 
         weighted = [0.1, 0.3, 0.4, 0.2] 
         cos_sim = []
-        emb1 = question_embs[question_id]
-        emb2 = emb_legal_data
-        cos_sim = util.cos_sim(emb1, emb2)
-        max_score = np.max(cos_sim)
+
+        for idx_2, model in enumerate(models):
+            emb1 = question_embs[idx_2][question_id]
+            emb2 = emb_legal_data[idx_2]
+            scores = util.cos_sim(emb1, emb2)
+            cos_sim.append(weighted[idx_2] * scores)
+        cos_sim = torch.cat(cos_sim, dim=0)
+        
+        cos_sim = torch.sum(cos_sim, dim=0).squeeze(0).numpy()
+        new_scores = doc_scores * cos_sim
+        max_score = np.max(new_scores)
 
         predictions = np.argpartition(new_scores, len(new_scores) - top_n)[-top_n:]
         new_scores = new_scores[predictions]
@@ -117,34 +145,29 @@ if __name__ == "__main__":
         map_ids = predictions[new_predictions]
         new_scores = new_scores[new_scores >= (max_score - range_score)]
 
-        if new_scores.shape[0] > 5:
-            predictions_2 = np.argpartition(new_scores, len(new_scores) - 5)[-5:]
-            map_ids = map_ids[predictions_2]
-            
-        pred_dict = {}
-        pred_dict["question_id"] = question_id
-        pred_dict["relevant_articles"] = []
+        # if new_scores.shape[0] > 5:
+        #     predictions_2 = np.argpartition(new_scores, len(new_scores) - 5)[-5:]
+        #     map_ids = map_ids[predictions_2]
+        true_positive = 0
+        false_positive = 0
         
         # post processing character error
         dup_ans = []
         for idx, idx_pred in enumerate(map_ids):
             pred = doc_refers[idx_pred]
-            law_id = pred[0]
-            article_id = pred[1]
             
-            if law_id.endswith("nd-cp"):
-                law_id = law_id.replace("nd-cp", "nđ-cp")
-            if law_id.endswith("nđ-"):
-                law_id = law_id.replace("nđ-", "nđ-cp")
-            if law_id.endswith("nð-cp"):
-                law_id = law_id.replace("nð-cp", "nđ-cp")
-            if law_id == "09/2014/ttlt-btp-tandtc-vksndtc":
-                law_id = "09/2014/ttlt-btp-tandtc-vksndtc-btc"
-            if law_id + "_" + article_id not in dup_ans:
-                dup_ans.append(law_id + "_" + article_id)
-                pred_dict["relevant_articles"].append({"law_id": law_id, "article_id": article_id})
-        pred_list.append(pred_dict)
+            for article in relevant_articles:
+                if pred[0] == article["law_id"] and pred[1] == article["article_id"]:
+                    true_positive += 1
+                else:
+                    false_positive += 1
+        precision = true_positive/(true_positive + false_positive + 1e-20)
+        recall = true_positive/actual_positive
+        f2 = calculate_f2(precision, recall)
+        total_precision += precision
+        total_recall += recall
+        total_f2 += f2
     
-    # extract result
-    with open('/result/submission.json', 'w') as outfile:
-        json.dump(pred_list, outfile)
+    print(f"Average F2: \t\t\t\t{total_f2/k}")
+    print(f"Average Precision: {total_precision/k}")
+    print(f"Average Recall: {total_recall/k}\n")
