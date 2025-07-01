d7984eceb21de3f19a64bda8dc2ab5b92ab2979c
phonghocode <ntp11892vt@gmail.com>
2025-07-01 21:15:14 +0700

    eval round 1

--- eval.py
+++ eval.py
@@ -210,6 +210,7 @@ if __name__ == "__main__":
         for i in np.arange(min_score, max_score, args.step):
             args.range_score = i
             avg_f2, avg_precision, avg_recall = evaluation(args, data, models, emb_legal_data, bm25, doc_refers, question_embs)
+            print(f"Score: {i}, F2: {avg_f2}, Precision: {avg_precision}, Recall: {avg_recall}")
             if best_f2 < avg_f2:
                 best_f2 = avg_f2
                 best_precision = avg_precision
