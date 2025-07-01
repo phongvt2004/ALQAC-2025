3dbf64c9d1b4859b9feb13c9ec95dc75bc61a81c
phonghocode <ntp11892vt@gmail.com>
2025-07-01 18:51:19 +0700

    eval round 1

--- eval.py
+++ eval.py
@@ -129,6 +129,7 @@ if __name__ == "__main__":
     num_eval = int(len(qid_list) * args.eval_size)
     eval_qid = qid_list[:num_eval]
     print("Start calculating results.")
+    k = num_eval
     for idx, item in tqdm(enumerate(data)):
         question_id = item["question_id"]
         if question_id not in eval_qid:
