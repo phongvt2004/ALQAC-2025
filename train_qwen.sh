291e3d42d97b1a1a30e37ade5cf861628396af63
phonghocode <ntp11892vt@gmail.com>
2025-07-14 05:58:16 +0700

    train qwen3

--- train_qwen.sh
+++ train_qwen.sh
@@ -5,5 +5,5 @@
 
 python train_sentence_transformer.py --pretrained_model Qwen/Qwen3-Embedding-0.6B \
 --step 100 --round 2 --saved_model qwen_saved_model \
---pair_data_path pair_data/bm_25_pairs_top50 --batch_size 16 --data_path ALQAC_2025_data \
+--pair_data_path pair_data/bm_25_pairs_top50 --batch_size 32 --data_path ALQAC_2025_data \
 --hub_model_id phonghoccode/ALQAC_2025_Qwen3_Embedding_top50
