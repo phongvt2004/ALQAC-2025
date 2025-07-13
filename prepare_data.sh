b58176686483f53922442fdd885ba833daa9b626
phonghocode <ntp11892vt@gmail.com>
2025-07-14 04:06:03 +0700

    train qwen3

---
+++ prepare_data.sh
@@ -0,0 +1,6 @@
+python process_data.py
+python process_data.py --data-path ALQAC_2025_data/additional_data/zalo --zalo
+python bm25_train.py
+python bm25_train.py --data-path ALQAC_2025_data/additional_data/zalo --zalo --save_path saved_model_zalo
+python bm25_create_pairs.py --top_pair 50
+python bm25_create_pairs.py --top_pair 50 --data_path ALQAC_2025_data/additional_data/zalo --zalo --save_pair_path pair_data_zalo --model_path saved_model_zalo/bm25_Plus_04_06_model_full_manual_stopword --saved_model_path saved_model_zalo
