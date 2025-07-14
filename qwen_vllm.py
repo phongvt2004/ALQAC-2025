d026894aced65383a896757f3cb6fe5b4e123e18
phonghocode <ntp11892vt@gmail.com>
2025-07-14 17:15:28 +0700

    train qwen3

--- qwen_vllm.py
+++ qwen_vllm.py
@@ -1,22 +1,95 @@
 # Requires vllm>=0.8.5
+import logging
+from typing import Dict, Optional, List
+
+import json
+import logging
+
 import torch
-import vllm
-from vllm import LLM
 
-model = LLM(model="Qwen/Qwen3-Embedding-0.6B", task="embed")
+from transformers import AutoTokenizer, is_torch_npu_available
+from vllm import LLM, SamplingParams
+from vllm.distributed.parallel_state import destroy_model_parallel
+import gc
+import math
+from vllm.inputs.data import TokensPrompt
 
-def get_detailed_instruct(task_description: str, query: str) -> str:
-    return f'Instruct: {task_description}\nQuery:{query}'
+number_of_gpu = torch.cuda.device_count()
+tokenizer = AutoTokenizer.from_pretrained('Qwen/Qwen3-Reranker-0.6B')
+model = LLM(model='Qwen/Qwen3-Reranker-0.6B', tensor_parallel_size=number_of_gpu, max_model_len=10000, enable_prefix_caching=True, gpu_memory_utilization=0.8)
+tokenizer.padding_side = "left"
+tokenizer.pad_token = tokenizer.eos_token
+suffix = "<|im_end|>\n<|im_start|>assistant\n<think>\n\n</think>\n\n"
+max_length=8192
+suffix_tokens = tokenizer.encode(suffix, add_special_tokens=False)
+true_token = tokenizer("yes", add_special_tokens=False).input_ids[0]
+false_token = tokenizer("no", add_special_tokens=False).input_ids[0]
+task = 'Given a legal query, retrieve relevant laws that answer the query'
+sampling_params = SamplingParams(temperature=0, 
+    max_tokens=1,
+    logprobs=20, 
+    allowed_token_ids=[true_token, false_token],
+)
 
-def qwen_embedding(query: str, type: str = "query") -> torch.Tensor:
-    task = 'Given a web search query, retrieve relevant passages that answer the query'
-    if type == "query":
-        instruction = get_detailed_instruct(task, query)
-    else:
-        # For retrieval documents, we can just use the query as is
-        instruction = query
+def format_instruction(instruction, query, doc):
+    text = [
+        {"role": "system", "content": "Judge whether the Document meets the requirements based on the Query and the Instruct provided. Note that the answer can only be \"yes\" or \"no\"."},
+        {"role": "user", "content": f"<Instruct>: {instruction}\n\n<Query>: {query}\n\n<Document>: {doc}"}
+    ]
+    return text
+
+def process_inputs(pairs, instruction, max_length, suffix_tokens):
+    messages = [format_instruction(instruction, query, doc) for query, doc in pairs]
+    messages =  tokenizer.apply_chat_template(
+        messages, tokenize=True, add_generation_prompt=False, enable_thinking=False
+    )
+    messages = [ele[:max_length] + suffix_tokens for ele in messages]
+    messages = [TokensPrompt(prompt_token_ids=ele) for ele in messages]
+    return messages
+
+def compute_logits(model, messages, sampling_params, true_token, false_token):
+    outputs = model.generate(messages, sampling_params, use_tqdm=False)
+    scores = []
+    for i in range(len(outputs)):
+        final_logits = outputs[i].outputs[0].logprobs[-1]
+        token_count = len(outputs[i].outputs[0].token_ids)
+        if true_token not in final_logits:
+            true_logit = -10
+        else:
+            true_logit = final_logits[true_token].logprob
+        if false_token not in final_logits:
+            false_logit = -10
+        else:
+            false_logit = final_logits[false_token].logprob
+        true_score = math.exp(true_logit)
+        false_score = math.exp(false_logit)
+        score = true_score / (true_score + false_score)
+        scores.append(score)
+    return scores
+
+def reranking(
+    query: str,
+    documents: List[str],
+    max_length: int = 8192,
+) -> Dict[str, float]:
+    """
+    Rerank documents based on a query using a pre-trained model.
     
-    model = LLM(model="Qwen/Qwen3-Embedding-0.6B", task="embed")
-    outputs = model.embed([instruction])
+    Args:
+        query (str): The search query.
+        documents (List[str]): List of documents to be reranked.
+        instruction (str): Instruction for the model.
+        max_length (int): Maximum length of input tokens.
     
-    return outputs[0].outputs.embedding
+    Returns:
+        Dict[str, float]: A dictionary with document indices as keys and their scores as values.
+    """
+    
+    
+    pairs = list(zip([query] * len(documents), documents))
+    inputs = process_inputs(pairs, task, max_length-len(suffix_tokens), suffix_tokens)
+    scores = compute_logits(model, inputs, sampling_params, true_token, false_token)
+    return {i: score for i, score in enumerate(scores)}
+
+def release_model():
+    destroy_model_parallel()
