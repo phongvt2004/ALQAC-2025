1f4169aff7cf0fab2e7c42783540f8b91ba011b7
phonghocode <ntp11892vt@gmail.com>
2025-07-17 05:15:12 +0700

    train

--- train_sentence_transformer.py
+++ train_sentence_transformer.py
@@ -121,121 +121,122 @@ if __name__ == '__main__':
         logging.info("Training finished!")
         
         print(f"Model saved to {output_path}")
-    if not args.zalo:
-        with open(os.path.join(args.data_path, "queries.json"), "r") as f:
-            queries = json.load(f)
-        qid_list = list(queries.keys())
-        random.seed(42)
-        random.shuffle(qid_list)
-        num_eval = int(len(qid_list) * args.eval_size)
-        eval_qid = qid_list[:num_eval]
-        for idx, pair in enumerate(save_pairs):
-            relevant = float(pair["relevant"])
-            qid = pair["qid"]
-            question = pair["question"]
-            document = pair["document"]
-            if args.wseg:
-                question = utils.word_segmentation(question)
-                document = utils.word_segmentation(document)
-            if qid not in eval_qid:
+    else:
+        if not args.zalo:
+            with open(os.path.join(args.data_path, "queries.json"), "r") as f:
+                queries = json.load(f)
+            qid_list = list(queries.keys())
+            random.seed(42)
+            random.shuffle(qid_list)
+            num_eval = int(len(qid_list) * args.eval_size)
+            eval_qid = qid_list[:num_eval]
+            for idx, pair in enumerate(save_pairs):
+                relevant = float(pair["relevant"])
+                qid = pair["qid"]
+                question = pair["question"]
+                document = pair["document"]
+                if args.wseg:
+                    question = utils.word_segmentation(question)
+                    document = utils.word_segmentation(document)
+                if qid not in eval_qid:
+                    train_examples["question"].append(question)
+                    train_examples["document"].append(document)
+                    train_examples["label"].append(relevant)
+                else:
+                    eval_examples["question"].append(question)
+                    eval_examples["document"].append(document)
+                    eval_examples["label"].append(relevant)
+        else:
+            save_eval_pairs = load_pair_data(args.pair_eval_path)
+            for idx, pair in enumerate(save_pairs):
+                relevant = float(pair["relevant"])
+                qid = pair["qid"]
+                question = pair["question"]
+                document = pair["document"]
+                if args.wseg:
+                    question = utils.word_segmentation(question)
+                    document = utils.word_segmentation(document)
                 train_examples["question"].append(question)
                 train_examples["document"].append(document)
                 train_examples["label"].append(relevant)
-            else:
+                
+            for idx, pair in enumerate(save_eval_pairs):
+                relevant = float(pair["relevant"])
+                qid = pair["qid"]
+                question = pair["question"]
+                document = pair["document"]
+                if args.wseg:
+                    question = utils.word_segmentation(question)
+                    document = utils.word_segmentation(document)
                 eval_examples["question"].append(question)
                 eval_examples["document"].append(document)
                 eval_examples["label"].append(relevant)
-    else:
-        save_eval_pairs = load_pair_data(args.pair_eval_path)
-        for idx, pair in enumerate(save_pairs):
-            relevant = float(pair["relevant"])
-            qid = pair["qid"]
-            question = pair["question"]
-            document = pair["document"]
-            if args.wseg:
-                question = utils.word_segmentation(question)
-                document = utils.word_segmentation(document)
-            train_examples["question"].append(question)
-            train_examples["document"].append(document)
-            train_examples["label"].append(relevant)
-            
-        for idx, pair in enumerate(save_eval_pairs):
-            relevant = float(pair["relevant"])
-            qid = pair["qid"]
-            question = pair["question"]
-            document = pair["document"]
-            if args.wseg:
-                question = utils.word_segmentation(question)
-                document = utils.word_segmentation(document)
-            eval_examples["question"].append(question)
-            eval_examples["document"].append(document)
-            eval_examples["label"].append(relevant)
-    print("Number of sample: ", len(train_examples["question"]))
-    print("Number of eval sample: ", len(eval_examples["question"]))
+        print("Number of sample: ", len(train_examples["question"]))
+        print("Number of eval sample: ", len(eval_examples["question"]))
 
-    train_dataset = Dataset.from_dict(train_examples)
-    eval_dataset = Dataset.from_dict(eval_examples)
-    with open(os.path.join(args.data_path, "corpus.json"), "r") as f:
-        corpus = json.load(f)
-    with open(os.path.join(args.data_path, "queries.json"), "r") as f:
-        queries = json.load(f)
-    with open(os.path.join(args.data_path, "relevant_docs.json"), "rb") as f:
-        relevant_docs = pickle.load(f)
-    
-    
+        train_dataset = Dataset.from_dict(train_examples)
+        eval_dataset = Dataset.from_dict(eval_examples)
+        with open(os.path.join(args.data_path, "corpus.json"), "r") as f:
+            corpus = json.load(f)
+        with open(os.path.join(args.data_path, "queries.json"), "r") as f:
+            queries = json.load(f)
+        with open(os.path.join(args.data_path, "relevant_docs.json"), "rb") as f:
+            relevant_docs = pickle.load(f)
+        
+        
 
-    eval_questions = list(eval_dataset["question"])
-    eval_documents = list(eval_dataset["document"])
-    eval_labels = list(eval_dataset["label"])
+        eval_questions = list(eval_dataset["question"])
+        eval_documents = list(eval_dataset["document"])
+        eval_labels = list(eval_dataset["label"])
 
-    evaluator = BinaryClassificationEvaluator(
-        sentences1=eval_questions,
-        sentences2=eval_documents,
-        labels=eval_labels,
-    )
+        evaluator = BinaryClassificationEvaluator(
+            sentences1=eval_questions,
+            sentences2=eval_documents,
+            labels=eval_labels,
+        )
 
-    hub_model_id = args.hub_model_id
-    
-    login(token=os.getenv("HUGGINGFACE_TOKEN"))
-    wandb.login(key=os.getenv("WANDB_API_KEY"))
-    wandb.init(project="ALQAC-2025", name=hub_model_id+"_run")
-    args = SentenceTransformerTrainingArguments(
-        # Required parameter:
-        output_dir=output_path,
-        # Optional training parameters:
-        num_train_epochs=args.epochs,
-        per_device_train_batch_size=args.batch_size,
-        per_device_eval_batch_size=args.batch_size*2,
-        gradient_accumulation_steps=10,
-        gradient_checkpointing=True,
-        learning_rate=args.lr,
-        warmup_ratio=0.1,
-        fp16=True,  # Set to False if you get an error that your GPU can't run on FP16
-        bf16=False,  # Set to True if you have a GPU that supports BF16
-        # batch_sampler=BatchSamplers.GROUP_BY_LABEL,  # MultipleNegativesRankingLoss benefits from no duplicate samples in a batch
-        # Optional tracking/debugging parameters:
-        eval_strategy="steps",
-        eval_steps=args.step*2,
-        save_strategy="steps",
-        save_steps=args.step*4,
-        logging_steps=args.step,
-        load_best_model_at_end=True,
-        prompts={"query": model.prompts["query"]} if "Qwen" in args.pretrained_model else None,
-        run_name=args.pretrained_model+"_run",  # Will be used in W&B if `wandb` is installed
-        push_to_hub=True if hub_model_id else False,
-        hub_model_id=hub_model_id if hub_model_id else None,
-    )
-    trainer = SentenceTransformerTrainer(
-        model=model,
-        args=args,
-        train_dataset=train_dataset,
-        eval_dataset=eval_dataset,
-        loss=loss,
-        evaluator=evaluator,
-    )
-    trainer.train()
-    wandb.finish()
-    logging.info("Training finished!")
-    
-    print(f"Model saved to {output_path}")
+        hub_model_id = args.hub_model_id
+        
+        login(token=os.getenv("HUGGINGFACE_TOKEN"))
+        wandb.login(key=os.getenv("WANDB_API_KEY"))
+        wandb.init(project="ALQAC-2025", name=hub_model_id+"_run")
+        args = SentenceTransformerTrainingArguments(
+            # Required parameter:
+            output_dir=output_path,
+            # Optional training parameters:
+            num_train_epochs=args.epochs,
+            per_device_train_batch_size=args.batch_size,
+            per_device_eval_batch_size=args.batch_size*2,
+            gradient_accumulation_steps=10,
+            gradient_checkpointing=True,
+            learning_rate=args.lr,
+            warmup_ratio=0.1,
+            fp16=True,  # Set to False if you get an error that your GPU can't run on FP16
+            bf16=False,  # Set to True if you have a GPU that supports BF16
+            # batch_sampler=BatchSamplers.GROUP_BY_LABEL,  # MultipleNegativesRankingLoss benefits from no duplicate samples in a batch
+            # Optional tracking/debugging parameters:
+            eval_strategy="steps",
+            eval_steps=args.step*2,
+            save_strategy="steps",
+            save_steps=args.step*4,
+            logging_steps=args.step,
+            load_best_model_at_end=True,
+            prompts={"query": model.prompts["query"]} if "Qwen" in args.pretrained_model else None,
+            run_name=args.pretrained_model+"_run",  # Will be used in W&B if `wandb` is installed
+            push_to_hub=True if hub_model_id else False,
+            hub_model_id=hub_model_id if hub_model_id else None,
+        )
+        trainer = SentenceTransformerTrainer(
+            model=model,
+            args=args,
+            train_dataset=train_dataset,
+            eval_dataset=eval_dataset,
+            loss=loss,
+            evaluator=evaluator,
+        )
+        trainer.train()
+        wandb.finish()
+        logging.info("Training finished!")
+        
+        print(f"Model saved to {output_path}")
 
