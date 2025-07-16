abdc018f1c243a43aab993f9a662d6ad41ec4bd2
phonghocode <ntp11892vt@gmail.com>
2025-07-17 01:39:34 +0700

    choose params

--- train.sh
+++ train.sh
@@ -1,11 +1,11 @@
-python eval.py --raw_data ALQAC_2025_data --find-best-score --hybrid --reranker AITeamVN/Vietnamese_Reranker
+python eval.py --raw_data ALQAC_2025_data --find-best-score --hybrid --reranker Qwen/Qwen3-Embedding-4B
 
 python train_sentence_transformer.py --pretrained_model AITeamVN/Vietnamese_Embedding_v2 \
---step 200 --round 1 --saved_model sbert_saved_model --eval_size 0.0\
+--step 200 --round 1 --saved_model sbert_saved_model --eval_size 0.0 \
 --pair_data_path pair_data/bm_25_pairs_top50 --batch_size 8 --data_path ALQAC_2025_data \
 --hub_model_id phonghoccode/ALQAC_2025_Embedding_final
 
 python train_sentence_transformer.py --pretrained_model huyydangg/DEk21_hcmute_embedding \
---step 200 --round 1 --wseg --saved_model sbert_wseg_saved_model --eval_size 0.0\
+--step 200 --round 1 --wseg --saved_model sbert_wseg_saved_model --eval_size 0.0 \
 --pair_data_path pair_data/bm_25_pairs_top50 --batch_size 8 --data_path ALQAC_2025_data \
 --hub_model_id phonghoccode/ALQAC_2025_Embedding_final_wseg
