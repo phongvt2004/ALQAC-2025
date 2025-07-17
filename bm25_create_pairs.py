12e3bc749f0b6231712fc531e8cbe7f7550d497f
phonghocode <ntp11892vt@gmail.com>
2025-07-17 15:26:37 +0700

    train rerank

--- bm25_create_pairs.py
+++ bm25_create_pairs.py
@@ -12,6 +12,7 @@ if __name__ == '__main__':
     parser.add_argument("--top_pair", default=20, type=int)
     parser.add_argument("--model_path", default="saved_model/bm25_Plus_04_06_model_full_manual_stopword", type=str)
     parser.add_argument("--zalo", action="store_true", help="use Zalo data")
+    parser.add_argument("--rerank", action="store_true", help="use data for rerank")
     parser.add_argument("--data_path", default="ALQAC_2025_data", type=str, help="path to input data")
     parser.add_argument("--save_pair_path", default="pair_data/", type=str, help="path to save pair sentence directory")
     parser.add_argument("--saved_model_path", default="saved_model", type=str, help="path to saved model data")
@@ -28,47 +29,91 @@ if __name__ == '__main__':
     corpus = json.load(open(os.path.join(args.data_path, "corpus.json")))
     save_pairs = []
     top_n = args.top_pair
-    for idx, item in tqdm(enumerate(data)):
-        question_id = item["question_id"] if not args.zalo else item["id"]
-        question = item["text"]
-        relevant_articles = item["relevant_articles"]
-        actual_positive = len(relevant_articles)
-        
-        tokenized_query = bm25_tokenizer(question)
-        doc_scores = bm25.get_scores(tokenized_query)
+    if args.rerank:
+        for idx, item in tqdm(enumerate(data)):
+            question_id = item["question_id"] if not args.zalo else item["id"]
+            question = item["text"]
+            relevant_articles = item["relevant_articles"]
+            actual_positive = len(relevant_articles)
+            
+            tokenized_query = bm25_tokenizer(question)
+            doc_scores = bm25.get_scores(tokenized_query)
 
-        predictions = np.argpartition(doc_scores, len(doc_scores) - top_n)[-top_n:]
+            predictions = np.argpartition(doc_scores, len(doc_scores) - (top_n+10))[-(top_n+10):]
 
-        # Save positive pairs
-        for article in relevant_articles:
+            # Save positive pairs
             save_dict = {}
-            save_dict["qid"] = question_id
-            save_dict["question"] = question
-            concat_id = article["law_id"] + " " + article["article_id"]
-            save_dict["document"] = corpus[concat_id]
-            save_dict["relevant"] = 1
-            save_pairs.append(save_dict)
-
-        # Save negative pairs
-        for idx, idx_pred in enumerate(predictions):
-            pred = doc_refers[idx_pred]
-
-            check = 0
+            save_dict["query"] = question
+            save_dict["pos"] = []
+            save_dict["neg"] = []
             for article in relevant_articles:
-                if pred[0] == article["law_id"] and pred[1] == article["article_id"]:
-                    check += 1
- 
-            if check == 0:
+
+                concat_id = article["law_id"] + " " + article["article_id"]
+                save_dict["pos"].append(corpus[concat_id])
+
+            # Save negative pairs
+            for idx, idx_pred in enumerate(predictions):
+                if idx >= top_n:
+                    break
+                pred = doc_refers[idx_pred]
+
+                check = 0
+                for article in relevant_articles:
+                    if pred[0] == article["law_id"] and pred[1] == article["article_id"]:
+                        check += 1
+
+                if check == 0:
+                    concat_id = pred[0] + " " + pred[1]
+                    save_dict["neg"].append(corpus[concat_id])
+            save_pairs.append(save_dict)
+        save_path = args.save_pair_path
+        os.makedirs(save_path, exist_ok=True)
+        with open(os.path.join(save_path, f"rerank_data_top{top_n}.jsonl"), "w") as f:
+            for item in save_pairs:
+                json_line = json.dumps(item, ensure_ascii=False)
+                f.write(json_line + "\n")
+    else:
+        for idx, item in tqdm(enumerate(data)):
+            question_id = item["question_id"] if not args.zalo else item["id"]
+            question = item["text"]
+            relevant_articles = item["relevant_articles"]
+            actual_positive = len(relevant_articles)
+            
+            tokenized_query = bm25_tokenizer(question)
+            doc_scores = bm25.get_scores(tokenized_query)
+
+            predictions = np.argpartition(doc_scores, len(doc_scores) - top_n)[-top_n:]
+
+            # Save positive pairs
+            for article in relevant_articles:
                 save_dict = {}
                 save_dict["qid"] = question_id
                 save_dict["question"] = question
-                concat_id = pred[0] + " " + pred[1]
+                concat_id = article["law_id"] + " " + article["article_id"]
                 save_dict["document"] = corpus[concat_id]
-                save_dict["relevant"] = 0
+                save_dict["relevant"] = 1
                 save_pairs.append(save_dict)
+
+            # Save negative pairs
+            for idx, idx_pred in enumerate(predictions):
+                pred = doc_refers[idx_pred]
+
+                check = 0
+                for article in relevant_articles:
+                    if pred[0] == article["law_id"] and pred[1] == article["article_id"]:
+                        check += 1
+    
+                if check == 0:
+                    save_dict = {}
+                    save_dict["qid"] = question_id
+                    save_dict["question"] = question
+                    concat_id = pred[0] + " " + pred[1]
+                    save_dict["document"] = corpus[concat_id]
+                    save_dict["relevant"] = 0
+                    save_pairs.append(save_dict)
                     
-    save_path = args.save_pair_path
-    os.makedirs(save_path, exist_ok=True)
-    with open(os.path.join(save_path, f"bm_25_pairs_top{top_n}"), "wb") as pair_file:
-        pickle.dump(save_pairs, pair_file)
-    print(len(save_pairs))
+        save_path = args.save_pair_path
+        os.makedirs(save_path, exist_ok=True)
+        with open(os.path.join(save_path, f"bm_25_pairs_top{top_n}"), "wb") as pair_file:
+            pickle.dump(save_pairs, pair_file)
+        print(len(save_pairs))
