b4de5d1908c71d8218f43c1a51d0fbe444ecf74f
phonghocode <ntp11892vt@gmail.com>
2025-07-19 01:38:38 +0700

    update

--- llm_final_output.py
+++ llm_final_output.py
@@ -15,7 +15,8 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
     torch_dtype="auto",
-    device_map="cuda:1"
+    device_map="auto",
+    attn_implementation="flash_attention_2"
 )
 
 
