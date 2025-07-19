bef6e778e1e85a39299d963b7700f08f0d6a6d08
phonghocode <ntp11892vt@gmail.com>
2025-07-19 08:34:37 +0700

    Revert "Reapply "update""
    This reverts commit a544d64bcf95fe8f952956190a5464bb2b64db17.

--- llm_final_output.py
+++ llm_final_output.py
@@ -50,42 +50,42 @@ def llm_generate(prompt: str):
     content = tokenizer.decode(output_ids[index:], skip_special_tokens=True).strip("\n")
     return content
 
-# with open("eval_results.json", "r") as f:
-#     data = json.load(f)
+with open("eval_results.json", "r") as f:
+    data = json.load(f)
 
-# for item in tqdm(data):
-#     predictions = item["predictions"]
-#     question = item["question"]
-#     prompt = f"Question: {question}\n{predictions}\nThis is outputs from a legal document retrieval system. Remove any irrelevant information and return a list of true predictions in the format: [{{'law_id': '...', 'article_id': '...'}}]. Only include predictions that are relevant to the question and help to answer the question."
-#     clean_text = llm_generate(prompt)
-#     output = ast.literal_eval(clean_text)
-#     item["relevant_articles"] = output
-# total_f2 = 0
-# total_precision = 0
-# total_recall = 0
-# for item in data:
-#     true_positive = 0
-#     false_positive = 0
-#     ground_truth = item["ground_truth"]
-#     relevant_articles = item["relevant_articles"]
-#     for article in relevant_articles:
-#         is_match = False
-#         for answer in ground_truth:
-#             if article["law_id"] == answer["law_id"] and article["article_id"] == answer["article_id"]:
-#                 true_positive += 1
-#                 is_match = True
-#                 break
-#         if not is_match:
-#             false_positive += 1
-#     precision = true_positive / (true_positive + false_positive + 1e-20)
-#     recall = true_positive / (len(ground_truth) + 1e-20)
-#     f2 = (5 * precision * recall) / (4 * precision + recall + 1e-20)
-#     total_f2 += f2
-#     total_precision += precision
-#     total_recall += recall
-# avg_f2 = total_f2 / len(data)
-# avg_precision = total_precision / len(data)
-# avg_recall = total_recall / len(data)
-# print(f"Average F2: {avg_f2}")
-# print(f"Average Precision: {avg_precision}")
-# print(f"Average Recall: {avg_recall}")
+for item in tqdm(data):
+    predictions = item["predictions"]
+    question = item["question"]
+    prompt = f"Question: {question}\n{predictions}\nThis is outputs from a legal document retrieval system. Remove any irrelevant information and return a list of true predictions in the format: [{{'law_id': '...', 'article_id': '...'}}]. Only include predictions that are relevant to the question and help to answer the question."
+    clean_text = llm_generate(prompt)
+    output = ast.literal_eval(clean_text)
+    item["relevant_articles"] = output
+total_f2 = 0
+total_precision = 0
+total_recall = 0
+for item in data:
+    true_positive = 0
+    false_positive = 0
+    ground_truth = item["ground_truth"]
+    relevant_articles = item["relevant_articles"]
+    for article in relevant_articles:
+        is_match = False
+        for answer in ground_truth:
+            if article["law_id"] == answer["law_id"] and article["article_id"] == answer["article_id"]:
+                true_positive += 1
+                is_match = True
+                break
+        if not is_match:
+            false_positive += 1
+    precision = true_positive / (true_positive + false_positive + 1e-20)
+    recall = true_positive / (len(ground_truth) + 1e-20)
+    f2 = (5 * precision * recall) / (4 * precision + recall + 1e-20)
+    total_f2 += f2
+    total_precision += precision
+    total_recall += recall
+avg_f2 = total_f2 / len(data)
+avg_precision = total_precision / len(data)
+avg_recall = total_recall / len(data)
+print(f"Average F2: {avg_f2}")
+print(f"Average Precision: {avg_precision}")
+print(f"Average Recall: {avg_recall}")
