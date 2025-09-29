861162f46a603a2c897530807a3351a6b4dbcc66
phonghocode <ntp11892vt@gmail.com>
2025-09-29 12:04:58 +0700

    update

--- retrieval/eval.py
+++ retrieval/eval.py
@@ -257,11 +257,11 @@ def evaluation(args, data, models, emb_legal_data, bm25, doc_refers, question_em
 def grid_search(args, data, models, emb_legal_data, bm25, doc_refers, question_embs):
     results = []
     # Prepare result logging
-    rerank_range_scores_list = [0.0, 1.0, 2.0]
+    rerank_range_scores_list = [0.05, 0.08, 0.1]
     retrieve_range_scores_list = {
-        "default": [10, 15],
+        #"default": [10, 15],
         "weighted_sum": [0.05, 0.08, 0.1],
-        "rrf": [0.001, 0.005, 0.01]
+        #"rrf": [0.001, 0.005, 0.01]
     }
     alphas = [0.3, 0.5, 0.7]
     for combine_type in tqdm(combine_types):
